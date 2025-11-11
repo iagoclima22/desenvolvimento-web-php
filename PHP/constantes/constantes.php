0f6f90779ea76943c0d95d6986f3c172f3039c25
Iago Lima <iagoclima22@gmail.com>
2025-11-11 12:48:18 -0300

    Update constantes.php

--- PHP/constantes/constantes.php
+++ PHP/constantes/constantes.php
@@ -1,4 +1,5 @@
 <?php
+// definindo, atribuindo e recuperando valores de uma constante
 define("NOME", "Iago Lima");
 echo NOME;
 
@@ -6,5 +7,18 @@ echo "<br>";
 
 const IDADE = 23;
 echo IDADE;
+
+// Constantes mágicas
+
+echo "<hr>";
+echo "A linha atual é a: ".__LINE__; // pega a linha atual
+echo "<hr>";
+
+echo "O caminho do arquivo atual é: ".__FILE__; // pega a linha atual
+echo "<hr>";
+
+echo "O nome do diretório atual é: ".__DIR__; // pega a linha atual
+echo "<hr>";
+
 
 ?>
