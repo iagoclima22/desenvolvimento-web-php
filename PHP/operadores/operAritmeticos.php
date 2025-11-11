c41c762d2db365dc369bbdb2b702424a7d56aa8f
Iago Lima <iagoclima22@gmail.com>
2025-11-11 17:30:57 -0300

    Create operAritmeticos.php

---
+++ PHP/operadores/operAritmeticos.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+    // Operadores Aritmeticos
+
+    $a = 4;
+    $b = 8;
+
+    // Adição +
+    $soma = $a + $b;
+    echo "A soma  a + b é: $soma <hr>";   
+    
+    // Multiplicação *
+    $mult = $a * $b;
+    echo "A multiplicação de  a x b é: $mult <hr>"; 
+
+    // Subtração -
+    $sub = $b - $a;
+    echo "A subtração de  a - b é: $sub <hr>"; 
+
+    // Divisão /
+    $div = $b / $a;
+    echo "A divisão de  a / b é: $div <hr>"; 
+
+    // Resto da divisão modulo %
+    $mod = $b % $a;
+    echo "O mod da divisão de a % b é: $mod <hr>"; 
+    
+    ?>
+    
+</body>
+</html>
