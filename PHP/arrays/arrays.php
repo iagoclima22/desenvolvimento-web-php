4b94ceadb54e1d74dc706cefabbcd7db5d1bfc18
Iago Lima <iagoclima22@gmail.com>
2025-11-11 14:57:20 -0300

    Create arrays.php

---
+++ PHP/arrays/arrays.php
@@ -0,0 +1,22 @@
+<?php 
+# Trabalhando com arrays
+
+$num = [1,2,3,4,5,7];
+
+# arrays associativos
+
+$frutas = [
+    "a" => "Banana",
+    "b" => "Abacaxi",
+    "c" => "Maçã",
+    "d" => "Uva"
+];
+
+echo "Array numerico valor do indice 0 é $num[0] \n";
+echo "Array assoc valor do indice 0 é $frutas[c] \n";
+
+print_r($num);
+print_r($frutas);
+
+
+?>
