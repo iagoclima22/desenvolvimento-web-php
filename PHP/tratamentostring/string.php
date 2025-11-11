17adf74e56a62eaac025e1c0f63118989eb6e6b1
Iago Lima <iagoclima22@gmail.com>
2025-11-11 14:04:52 -0300

    Update string.php - lower, upper, capitalize

--- PHP/tratamentostring/string.php
+++ PHP/tratamentostring/string.php
@@ -1,26 +1,37 @@
 <?php 
 
-    $string = "  Eu sou uma string  ";
-    $string1 = "EU SOU UMA STRING";
-    $string2 = "  Eu sou uma nova string";
-    $string3 = "Eu sou uma nova string  ";
-    $codigo = "2";
+$string = "  Eu sou uma string  ";
+$string1 = "EU SOU UMA STRING";
+$string2 = "  Eu sou uma nova string";
+$string3 = "Eu sou uma nova string  ";
+$codigo = "2";
 
-    # trim - ltrim - rtrim : responsável por eliminar os espaços em branco
-    $trim = trim($string);
-    $ltrim = ltrim($string);
-    $rtrim = rtrim($string);
-    echo "$string \n";
-    echo "$trim \n";
-    echo "$ltrim \n";
-    echo "$rtrim \n";
+$lower = strtolower($string1);
+$upper = strtoupper($string3);
+$cap =  ucfirst(strtolower($string1));
+$cap2 =  ucwords(strtolower($string1));
 
-    # strlen : mostrar a quantidade de caracteres de uma string
-    $len = strlen($string3);
-    echo "A string possui $len caracteres \n";
-    # substr - mostrar um determinado numero de caracteres
-    $sbs = substr($strings, 4, 9);
-    echo "$sbs \n";
+echo "Minusculo: $lower \n";
+echo "Maiusculo: $upper \n";
+echo "Captalize: $cap \n";
+echo "Captalize: $cap2 \n";
+
+
+# trim - ltrim - rtrim : responsável por eliminar os espaços em branco
+$trim = trim($string);
+$ltrim = ltrim($string);
+$rtrim = rtrim($string);
+echo "$string \n";
+echo "$trim \n";
+echo "$ltrim \n";
+echo "$rtrim \n";
+
+# strlen : mostrar a quantidade de caracteres de uma string
+$len = strlen($string3);
+echo "A string possui $len caracteres \n";
+# substr - mostrar um determinado numero de caracteres
+$sbs = substr($strings, 4, 9);
+echo "$sbs \n";
 
 
 ?>
