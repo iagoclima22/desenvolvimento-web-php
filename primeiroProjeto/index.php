9a7a6e19847747e538e330c099cdfc6b2111e6fc
Iago Lima <iagoclima22@gmail.com>
2025-11-11 13:08:34 -0300

    Create index.php

---
+++ primeiroProjeto/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    echo "Escreva o seu nome: ";
+    $nome = readline();
+
+    echo "Informe sua idade: ";
+    $idade = intval(readline());
+
+    echo "Informe sua profissão: ";
+    $profissao = readline();
+
+    echo "Olá, $nome é $profissao e tem $idade anos e idade";
+
+?>
