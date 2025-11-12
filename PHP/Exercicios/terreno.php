5bc906c5355d7c2273ee09c5ada10ba69c313c2c
Iago Lima <iagoclima22@gmail.com>
2025-11-12 17:05:42 -0300

    Create terreno.php

---
+++ PHP/Exercicios/terreno.php
@@ -0,0 +1,15 @@
+<?php 
+echo "Digite a largura do terreno: ";
+$largura = (float)readline();
+echo "Digite o comprimento do terreno: ";
+$comprimento = (float)readline();
+echo "Digite o valor do metro quadrado: ";
+$valorM2 = (float)readline();
+
+$area = $largura * $comprimento;
+$preco = $area * $valorM2;
+
+echo "Area do terreno = ".number_format($area, 2). "\n";
+echo "Preco do terreno = ".number_format($preco, 2). "\n";
+
+?>
