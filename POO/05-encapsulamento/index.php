a6ac8305c70f234d9dd7b85eac85ae3d8fbea663
Iago Lima <iagoclima22@gmail.com>
2025-11-27 16:38:02 -0300

    Update index.php

--- POO/05-encapsulamento/index.php
+++ POO/05-encapsulamento/index.php
@@ -3,9 +3,12 @@
 require_once "Carro.php";
 
 $c = new Carro("Civic", "Honda", "2020");
-echo $c->marca."<br>";
-echo $c->modelo."<br>";
-echo $c->ano."<br>";
+
+echo $c->getMarca()."<br>";
+echo $c->getModelo()."<br>";
+echo $c->getAno( )."<br>";
+
+$c->setMarca("Fiat"); 
 
 echo "<br> <hr> <br>";
 
