417bbb4f09c868deb29fa2894985ee339ed07aaf
Iago Lima <iagoclima22@gmail.com>
2025-11-27 16:37:31 -0300

    Update Carro.php

--- POO/05-encapsulamento/Carro.php
+++ POO/05-encapsulamento/Carro.php
@@ -1,8 +1,8 @@
 <?php
 class Carro{
-    public string $modelo;
-    public string $marca;
-    public string $ano;
+    private string $modelo;
+    private string $marca;
+    private string $ano;
 
     public function __construct(string $modelo, string $marca, string $ano){
         $this->modelo = $modelo;
@@ -10,10 +10,35 @@ class Carro{
         $this->ano = $ano;
     }
 
+    // metodos get e set
+    
+    public function getMarca(){
+        return $this->marca; 
+    }
+
+    public function getModelo(){
+       return $this->modelo; 
+    }
+
+    public function getAno() {
+        return $this->ano;
+    }
+
+    public function setMarca($marca) {
+        return $this->marca = $marca;
+    }
+
+    public function setModelo($modelo) {
+        return $this->modelo = $modelo;
+    }
+
+    public function setAno($ano) {
+        return $this->ano = $ano;
+    }
+
     public function exibirDetalhesCarro(){
         echo "O carro ".$this->modelo." é da marca ".$this->marca." do ano ".$this->ano;
     }
 
 }
-
 ?>
